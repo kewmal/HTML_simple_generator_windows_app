7f6a338caa5a547e2b759e920c8edc595dd4d8ed
Kewin Malicki <108068941+kewmal@users.noreply.github.com>
2023-10-02 17:54:56 +0200

    Add files via upload

---
+++ projekt/projekt/MainForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 02.10.2023
+ * Time: 16:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace projekt
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.SystemColors.Menu;
+			this.button1.Location = new System.Drawing.Point(12, 38);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(237, 103);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Generowanie kodu HTML";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(237, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 147);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(237, 41);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Informacje o oprogramowaniu";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(260, 200);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.Name = "MainForm";
+			this.Text = "Generator kodu HTML";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ projekt/projekt/MainForm.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Paweł Malicki
+ * Date: 02.10.2023
+ * Time: 16:15
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace projekt
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			textBox1.Text="<html><head></head><body><h1>Wygenerowana strona internetowa</h1></body></html>";
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void Button2Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Zrobione przez:\nPaweł Kewin Malicki");
+		}
+	}
+}
